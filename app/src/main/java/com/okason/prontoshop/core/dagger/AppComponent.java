06f691da03deeaa6fb54440bf2f882d4af18bff0
LUIS-PC\Luis <schizophrenia.androi@gmail.com>
2017-07-07 18:16:21 -0500

    testing in minoni

--- app/src/main/java/com/okason/prontoshop/core/dagger/AppComponent.java
+++ app/src/main/java/com/okason/prontoshop/core/dagger/AppComponent.java
@@ -1,8 +1,34 @@
 package com.okason.prontoshop.core.dagger;
 
+import com.okason.prontoshop.common.MainActivity;
+import com.okason.prontoshop.common.ShoppingCart;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * Created by Gerardo on 18/06/2017.
  */
 
+@Singleton
+
+/*
+Used on an interface. This interface is used by Dagger 2
+ to generate code which uses the modules to fulfill the
+ requested dependencies.
+ */
+
+@Component(
+        modules = {
+                AppModule.class,
+                ShoppingCartModule.class,
+                BusModule.class
+        }
+)
 public interface AppComponent {
+    void inject(MainActivity activity);
+
+    void inject(ShoppingCart cart);
+
 }
--- app/src/main/java/com/okason/prontoshop/core/dagger/AppModule.java
+++ app/src/main/java/com/okason/prontoshop/core/dagger/AppModule.java
@@ -1,12 +1,48 @@
 package com.okason.prontoshop.core.dagger;
 
+import android.content.Context;
+
+import com.okason.prontoshop.core.ProntoShopApplication;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by Gerardo on 18/06/2017.
  */
 
+
+/*
+*
+* Used on classes which contains methods annotated with @Provides.
+*
+*/
+@Module
 public class AppModule {
 
+    private final ProntoShopApplication app;
 
+    public AppModule(ProntoShopApplication app) {
+        this.app = app;
+    }
 
+    /*
+    Can be used on methods in classes annotated with @Module and is used for methods which provides
+    objects for dependencies injection.
+    * */
+    @Provides
 
+    /*Single instance of this provided object is created and shared.*/
+    @Singleton
+    public ProntoShopApplication provideApp() {
+        return app;
+    }
+
+    @Provides
+    @Singleton
+    public Context provideContext() {
+        return app;
+    }
 }
